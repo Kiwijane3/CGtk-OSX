ef9f7e3ca6915db3e087c4755fc972e48efd5e36
kiwijane3 <janef0421@gmail.com>
2020-03-19 15:51:00 +1300

    Updated to swift tools version 5.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "CGtk-OSX",
     pkgConfig: "gtk+-3.0",
-    providers: [.Brew("gtk+3")]
+    providers: [.brew(["gtk+3"])]
 )
